1f01e4b8bbc76f21a9ddfedb030a60784444c35e
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-12-12 22:15:24 +0200

    initial

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/test-sync', syncFunc);
+function syncFunc (req, res) {
+    res.send('Hello sync');
+}
+
+// comment to disable error throws
+// app.get('/test-async', asyncFunc);
+async function asyncFunc (req, res) {
+    const value = await someAPICallMock();
+    res.send(value);
+}
+
+function someAPICallMock () {
+    return new Promise(resolve => {
+        setTimeout(() => resolve('Success'), 500);
+    });
+}
+
+app.listen(8080);
+console.log('Server is started on 8080');
